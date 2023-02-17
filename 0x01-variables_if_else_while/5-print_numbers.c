0dc8abf57d28376baffab9ce8273e0d96a7dea20
OmbokJustus <ombokjustus@gmail.com>
2023-02-17 08:12:19 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num <10; num++);
+	{
+		printf("%d", nums);
+	}
+	putchar('\n');
+	return (0);
+}
