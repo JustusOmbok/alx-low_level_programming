2316a2636b5c7e7396d77a2f961550c62c1744f5
OmbokJustus <ombokjustus@gmail.com>
2023-02-18 03:21:11 -0800

    betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,11 +4,11 @@
  * numbers separated by , followed by a space
  * the two digits are different
  */
-int main (void)
+int main(void)
 {
 	int c;
 	int d = 0;
-	
+
 	while (d < 10)
 	{
 		c = 0;
@@ -19,7 +19,7 @@ int main (void)
 				putchar('0' + d);
 				putchar('0' + c);
 
-				if(c + d != 17)
+				if (c + d != 17)
 				{
 					putchar(',');
 					putchar(' ');
