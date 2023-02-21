82ef3623cf26fd2a3a96275e5e441dee08db0d7c
OmbokJustus <ombokjustus@gmail.com>
2023-02-21 13:05:02 -0800

    checking absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - determine the absolute value of an integer
+ * @i: variable to determine absolute value of an integer
+ * Return: i if successful
+ */
+
+int _abs(int i)
+{
+if (i < 0)
+i *= -1;
+return (i);
+}
