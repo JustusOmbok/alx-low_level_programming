706375319fddf1ab0426a666756ede7fc47bc05b
OmbokJustus <ombokjustus@gmail.com>
2023-02-22 03:01:04 -0800

    printing 1 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints out numbers 1 to 98
+ *@n: variable to determine if input is greater, less or equal than 98
+ *
+ */
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n >= 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+printf(",");
+else
+printf("\n");
+}
+}
+else if (n < 98)
+{
+for (; n <= 98; n++)
+{
+printf("%d", n)
+if (n !=98)
+printf(",");
+else
+printf("\n");
+}
+}
+else
+{
+print("%d\n", n);
+}
+}
+
