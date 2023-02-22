3cba3ea01aa6ea445e61cc771881b2c20123db3d
OmbokJustus <ombokjustus@gmail.com>
2023-02-22 03:04:48 -0800

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,7 +24,7 @@ else if (n < 98)
 {
 for (; n <= 98; n++)
 {
-printf("%d", n)
+printf("%d", n);
 if (n !=98)
 printf(",");
 else
@@ -33,7 +33,7 @@ printf("\n");
 }
 else
 {
-print("%d\n", n);
+printf("%d\n", n);
 }
 }
 
