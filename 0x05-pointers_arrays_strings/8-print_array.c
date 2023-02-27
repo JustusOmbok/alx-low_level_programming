5e3f56f79bdf403aeb1157ebc3601f1481280d2d
OmbokJustus <ombokjustus@gmail.com>
2023-02-27 07:34:58 -0800

    faulty pintf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,7 @@ void print_array(int *a, int n)
 	while (count < n)
 	{
 		if (count != (n-1))
-			print("%i, ", a[count]);
+			printf("%i, ", a[count]);
 		else
 			printf("%i", a[count]);
 		count++;
