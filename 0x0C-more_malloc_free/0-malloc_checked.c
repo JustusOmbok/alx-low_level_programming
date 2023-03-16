f7a803c6ad9f659d1757b3d1b386a4e35f148c30
OmbokJustus <ombokjustus@gmail.com>
2023-03-16 08:16:04 -0700

    alloatesc memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates mem using malloc
+ * @b: size of mem to be allocated
+ *
+ * Return: pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
