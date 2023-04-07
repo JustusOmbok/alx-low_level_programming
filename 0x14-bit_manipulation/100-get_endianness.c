15dd56a259800c9e9d55e0c85f89dd5002cf512c
JustusOmbok <ombokjustus@gmail.com>
2023-04-07 03:11:31 -0700

    checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 1 if little endian, 0 if big endian
+ */
+
+int grt_endianness(void)
+{
+	int value = 1;
+	char *ptr = (char*)&value;
+
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
