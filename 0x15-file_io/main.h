f4536fa9f77560c52560ac58e02c9aadda5febde
JustusOmbok <ombokjustus@gmail.com>
2023-04-09 06:06:27 -0700

    prints num of letters

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,12 +2,9 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <stdio.y>
-#include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
+#include <stddef.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
