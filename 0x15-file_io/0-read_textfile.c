ec98f539d11331a6147994e2e0258d5bf3012ce0
JustusOmbok <ombokjustus@gmail.com>
2023-04-09 02:09:15 -0700

    prints number of letters

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints to POSIX stdout
+ * @letters: number of letters it should read and print
+ * @filename: name of the file
+ * Return: actual number of letters if success, 0 if file is null
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+	{
+		return (0);
+	}
+	char *buffer = (char *) malloc(letters * sizeof(char));
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+	ssize_t read_count = fread(buffer, sizeof(char), letters, file);
+
+	if (ferror(file) || read_count != letters)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	ssize_t write_count = fwrite(buffer, sizeof(char), read_count, stdout);
+
+	if (write_count != read_count)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	fclose(file);
+	free(buffer);
+	return (read_count);
+}
