efe4f46e01aff51fb21c1a420c8073448d0c4bd0
JustusOmbok <ombokjustus@gmail.com>
2023-04-10 22:41:51 -0700

    gets endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * get_endianness - checks the endianness
@@ -7,7 +7,7 @@
  * Return: 1 if little endian, 0 if big endian
  */
 
-int grt_endianness(void)
+int get_endianness(void)
 {
 	int value = 1;
 	char *ptr = (char *)&value;
@@ -16,3 +16,6 @@ int grt_endianness(void)
 	if (*ptr == 1)
 	{
 		return (1);
+	}
+	return (0);
+}
