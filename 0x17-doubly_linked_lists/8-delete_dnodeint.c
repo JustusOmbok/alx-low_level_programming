efe8477248a60942e5c497902341a94860f1c75c
JustusOmbok <ombokjustus@gmail.com>
2023-06-15 05:56:28 -0700

    deletes node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * delete_dnodeint_at_index - inserts at given index
  * @head: start of list
@@ -11,17 +12,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 
 	if (*head == NULL)
+	{
 		return (-1);
+	}
 
 	if (index == 0)
 	{
 		*head = (*head)->next;
-
 		if (*head != NULL)
 			(*head)->prev = NULL;
 
-			free(current);
-			return (1);
+		free(current);
+		return (1);
 	}
 
 	while (index > 0)
