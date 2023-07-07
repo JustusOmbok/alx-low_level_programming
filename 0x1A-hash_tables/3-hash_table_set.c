eef8efcb23f78b4d1977fff8887d811c67aa23b9
JustusOmbok <ombokjustus@gmail.com>
2023-07-06 23:18:11 -0700

    adds value

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set -an element is added to hash table
+ * @ht: hash table to add value/key
+ * @key: key string
+ * @value: value string
+ * Return: 1 success, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
