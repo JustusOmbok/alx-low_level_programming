a622fc902c046a1f5b0bf98b9dfc2fde1acf6878
JustusOmbok <ombokjustus@gmail.com>
2023-10-09 14:41:22 +0300

    linear search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches for a value in a sorted array
+ * @array: Pointer to first element of the array to be searched
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The index where the value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int mid;
+	int i;
+
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+
+		if (array[mid] == value)
+			return mid;
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return -1;
+}
