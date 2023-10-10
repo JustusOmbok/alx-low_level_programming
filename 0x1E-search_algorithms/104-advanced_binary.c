ef4ab33cb51c8c98d7ea012b4ed105f4dcaf297a
JustusOmbok <ombokjustus@gmail.com>
2023-10-10 20:39:15 +0300

    binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+
+/**
+ * prints_array - Array is printed within a range
+ * @arrs: Array's pointer
+ * @starts: The range's starting index
+ * @ends: The range's ending index
+ */
+void prints_array(int *arrs, int starts, int ends)
+{
+	int i = 0;
+
+	printf("Searching in array: ");
+	while (starts <= ends)
+	{
+		if (i > 0)
+			printf(", ");
+		i = starts++;
+		printf("%d", arrs[i++]);
+	}
+	printf("\n");
+}
+
+/**
+ * recursion_search - Searches for a value recursively
+ * @arrs: Array's pointer
+ * @low: Lower end of the array
+ * @high: High end of the array
+ * @val: The value to be searched
+ *
+ * Return: Where the value is located
+ */
+
+int recursion_search(int *arrs, int low, int high, int val)
+{
+	int mid;
+
+	if (high >= low)
+	{
+		mid = low + (high - low) / 2;
+		prints_array(arrs, low, high);
+
+		if (arrs[mid] == val)
+		{
+			if (mid != 0 && arrs[mid - 1] == val)
+				return (recursion_search(arrs, low, mid, val));
+			return (mid);
+		}
+
+		if (arrs[mid] > val)
+			return (recursion_search(arrs, low, mid, val));
+
+		return recursion_search(arrs, mid + 1, high, val);
+	}
+	return -1;
+}
+
+/**
+ * advanced_binary - Searches for a value in a sorted array
+ * @array: Array to search in
+ * @size: Size of the array
+ * @value: The value to search for
+ *
+ * Return: Index where value is located
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array && size)
+		return (recursion_search(array, 0, (int)size -1, value));
+	return -1;
+}
